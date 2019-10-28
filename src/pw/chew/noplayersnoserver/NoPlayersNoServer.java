a357e3d06b5e2e585fa992aec936c22bf6f5cde4
Chew <chew@chew.pw>
2019-10-28 15:00:26 -0500

    the basic plugin skeleton

---
+++ src/pw/chew/noplayersnoserver/NoPlayersNoServer.java
@@ -0,0 +1,13 @@
+package pw.chew.noplayersnoserver;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NoPlayersNoServer {
+	// Fired when plugin is first enabled
+    public void onEnable() {
+    }
+    // Fired when plugin is disabled
+    public void onDisable() {
+
+    }
+		
+}
