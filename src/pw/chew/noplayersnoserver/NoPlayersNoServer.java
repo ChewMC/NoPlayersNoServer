da20f7386b2ce8584f9f14f5c2d92ceb3b11aa58
Chew <chew@chew.pw>
2019-10-29 06:41:39 -0500

    register events on enable

--- src/pw/chew/noplayersnoserver/NoPlayersNoServer.java
+++ src/pw/chew/noplayersnoserver/NoPlayersNoServer.java
@@ -5,6 +5,7 @@ public class NoPlayersNoServer extends JavaPlugin {
 	// Fired when plugin is first enabled
     public void onEnable() {
       this.getCommand("npns").setExecutor(new HelpCommand());
+      getServer().getPluginManager().registerEvents(new ByeByePlayer(), this);
     }
     // Fired when plugin is disabled
     public void onDisable() {
