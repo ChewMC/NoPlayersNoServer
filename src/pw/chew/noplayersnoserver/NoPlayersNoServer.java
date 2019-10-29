09bb889cb9be0e80525879457aade89e67c94cd4
Chew <chew@chew.pw>
2019-10-29 07:42:24 -0500

    add listener on enable

--- src/pw/chew/noplayersnoserver/NoPlayersNoServer.java
+++ src/pw/chew/noplayersnoserver/NoPlayersNoServer.java
@@ -2,14 +2,15 @@ package pw.chew.noplayersnoserver;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NoPlayersNoServer extends JavaPlugin {
-	// Fired when plugin is first enabled
-    public void onEnable() {
-      this.getCommand("npns").setExecutor(new HelpCommand());
-      getServer().getPluginManager().registerEvents(new ByeByePlayer(), this);
-    }
-    // Fired when plugin is disabled
-    public void onDisable() {
+  // Fired when plugin is first enabled
+  TimerUntilBye timer = new TimerUntilBye();
+  public void onEnable() {
+    this.getCommand("npns").setExecutor(new HelpCommand());
+    getServer().getPluginManager().registerEvents(new ByeByePlayer(), this);
+    System.out.println("Timer won't activate until someone leaves.");
+  }
+  // Fired when plugin is disabled
+  public void onDisable() {
 
-    }
-
+  }
 }
