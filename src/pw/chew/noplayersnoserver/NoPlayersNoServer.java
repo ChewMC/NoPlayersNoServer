9c945112875fb8fc369a2074f310e346e49e7469
Chew <chew@chew.pw>
2019-10-28 21:17:16 -0500

    add and register main help command

--- src/pw/chew/noplayersnoserver/NoPlayersNoServer.java
+++ src/pw/chew/noplayersnoserver/NoPlayersNoServer.java
@@ -1,13 +1,14 @@
 package pw.chew.noplayersnoserver;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class NoPlayersNoServer {
+public class NoPlayersNoServer extends JavaPlugin {
 	// Fired when plugin is first enabled
     public void onEnable() {
+      this.getCommand("npns").setExecutor(new HelpCommand());
     }
     // Fired when plugin is disabled
     public void onDisable() {
 
     }
-		
+
 }
